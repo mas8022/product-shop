b638da10665786b933f61b0c28fb2dab59a6cae3
masproo22 <m.abbaszadeh2002@gmail.com>
2025-01-15 14:48:04 +0330

    fix

--- configs/db.ts
+++ configs/db.ts
@@ -6,10 +6,14 @@ async function connectToDb(): Promise<string> {
       return "Already connected to the database.";
     }
 
-    await mongoose.connect("mongodb://localhost:27017/berengsar");
+    await mongoose.connect(
+      "mongodb://root:rjuiH99HMTZvmA167aDyZ3rg@hotaka.liara.cloud:31145/my-app?authSource=admin&replicaSet=rs0&directConnection=true"
+    );
     return "Connected to the database.";
   } catch (error) {
-    throw new Error(error instanceof Error ? error.message : "Unknown error occurred.");
+    throw new Error(
+      error instanceof Error ? error.message : "Unknown error occurred."
+    );
   }
 }
 
